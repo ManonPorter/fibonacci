a779d8634c0e5c328c1d39de97011f96c79224a6
Manon Porter <cosmobeatles@gmail.com>
2021-04-14 16:00:15 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 'use strict';
+const memo = new Map(); //memoという名のMapを新規作成
+memo.set(0, 0)//(値が０な０という名のキーを設定＝0番目用)
+memo.set(1,1)//(値が1の1という名のキーを設定＝１番目用)
 function fib(n){
-    if(n === 0){
-        return 0;
-    } //n=0なら０を返す　
-    else if (n === 1){
-        return 1;
-    } //n=1なら1を返す
-    return fib(n-1) + fib(n-2); //それ以外ならnの一つ前と2つ前の数を足し合わせた値を返す
+        if (memo.has(n)){
+		return memo.get(n); //もしmemoにすでにnのキーがあったらその値をそのまま返す
+	const value = fib(n-1) + fib(n-2);
+	memo.set(n, value);
+	return value;
 }
-
+}
 const length = 40;
 for (let i = 0; i<= length; i++){
-    console.log(fib(i));
+	console.log(fib(i))
 }
